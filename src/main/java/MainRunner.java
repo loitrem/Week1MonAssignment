93e161cc6053eb9ef90d846e38f3e2ce6321b354
loitrem <ski_fix@hotmail.com>
2021-04-12 12:51:47 -0400

    Stage 1 commit

---
+++ src/main/java/MainRunner.java
@@ -0,0 +1,15 @@
+public class MainRunner {
+    public static void main(String[] args) {
+
+    Stage1 s1 = new Stage1();
+
+
+    for (int i=0; i<s1.output.length; i++){
+            System.out.println(s1.output[i]);
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/Stage1.java
@@ -0,0 +1,13 @@
+public class Stage1 {
+
+    String[] output = {"Starting to make a coffee\n" +
+            "Grinding coffee beans\n" +
+            "Boiling water\n" +
+            "Mixing boiled water with crushed coffee beans\n" +
+            "Pouring coffee into the cup\n" +
+            "Pouring some milk into the cup\n" +
+            "Coffee is ready!"};
+
+
+
+}
