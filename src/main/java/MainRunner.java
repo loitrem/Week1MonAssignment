ed41b0519a8710c61dba2468fe99e332a745b22c
loitrem <ski_fix@hotmail.com>
2021-04-12 13:03:11 -0400

    Stage 1 commit

--- src/main/java/MainRunner.java
+++ src/main/java/MainRunner.java
@@ -2,12 +2,14 @@ public class MainRunner {
     public static void main(String[] args) {
 
     Stage1 s1 = new Stage1();
-
+    Stage2 s2 = new Stage2();
 
     for (int i=0; i<s1.output.length; i++){
             System.out.println(s1.output[i]);
         }
 
+        s2.getCups();
+        System.out.println(s2.getWater() + "\n" + s2.getMilk() + "\n" + s2.getCoffee());
 
 
 
